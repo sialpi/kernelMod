ada25c99abb8584c95e27ca52d4f32a130cf6091
sialpi <alessandro.scialpi@hotmail.com>
2026-03-29 16:14:15 +0200

    better scripts

--- compile-kernel.sh
+++ compile-kernel.sh
@@ -1,2 +1,4 @@
 cd linux-6.9/
+make defconfig
+make kvm_guest.config
 make -j$(nproc) CC="gcc -std=gnu11"
--- install-kernel.sh
+++ install-kernel.sh
@@ -1,3 +1,4 @@
 wget https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.9.tar.xz
 tar -xvf linux-6.9.tar.xz
+rm -rf linux-6.9.tar.xz
 cd linux-6.9
